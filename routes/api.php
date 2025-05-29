77fb0219f606023dea2e06a2997b4c81157bbe98
codebaradev <bahnasep@gmail.com>
2025-05-30 03:47:22 +0800

    refactor: Delete Breeze API

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,16 @@ use App\Http\Controllers\StudentController;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResource('students', StudentController::class);
+
