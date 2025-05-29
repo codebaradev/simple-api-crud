c5366d03fd2d7b92972106f7b0fbddd8bb35ff83
codebaradev <bahnasep@gmail.com>
2025-05-29 22:33:48 +0800

    feat: Setup auth

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'Laravel' => app()->version(),
+        'urls' => [
+            'login' => '/login',
+            'register' => '/register'
+        ]
+    ];
 });
+
+require __DIR__.'/auth.php';
